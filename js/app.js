a43c14db33ccda9f427e389ac9186239d79a4531
Kurt Poquette <kxp1607@rit.edu>
2015-08-01 16:31:56 -0500

    Checks guesses correctly. Restart and guess tracking needed

--- js/app.js
+++ js/app.js
@@ -1,6 +1,53 @@
+//Number of attempted guesses
+var numTry;
+var randNum; 
 
-$(document).ready(function(){
+function chooseNum(){
+	//Choose random number between 0 and 1 then multiply by 100
+  	var num = Math.random() * 100;
+  	//Parse the number to an int to remove any decimal places (radix set to 10 for decimal)
+  	num = parseInt(num, 10);
+  	//Makes num between 1-100 rather than 0-99
+  	num = num + 1;
+
+  	return num;
+}
+
+function newGame() {
+	//Reset number of tries to 0
+  	numTry = 0;
+  	randNum = chooseNum();
+  	//Clear the list of guesses
+  	$('#guessList').empty();
+}
+
+function checkGuess(guess){
+	var diff = Math.abs(guess - randNum);
+
+	if(diff === 0){
+		$("#feedback").text("Correct!");
+	} else if(diff >= 1 && diff <= 10) {
+		$("#feedback").text("Very Hot!");
+	} else if(diff >= 11 && diff <= 20) {
+		$("#feedback").text("Hot!");
+	} else if(diff >= 21 && diff <= 30) {
+		$("#feedback").text("Warm");
+	} else if(diff >= 31 && diff <= 50) {
+		$("#feedback").text("Cold");
+	} else if(diff >= 51) {
+		$("#feedback").text("Ice Cold!");
+	} else {
+		$("#feedback").text("An error has occured, reload page");
+	}
+
+	numTry = numTry + 1;
+	$("#count").text(numTry);
+ }
+
+$(document).ready(function () {
 	
+	newGame();
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -12,6 +59,19 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	$("form").submit(function (e) {
+  		e.preventDefault();
+  		//Check for valid input
+  		//TODO function for validating input
+  		var userGuess = $("#userGuess").val();
+  		console.log(userGuess);
+  		if(!userGuess){
+  			$("#feedback").text("Enter a valid number please");
+  		} else {
+  			checkGuess(parseInt($("#userGuess").val(), 10));
+  		}
+  	});
+
+
+
 });
-
-
